3002bb844d73e7558d69f2c94f739e7d3c869295
Kris2327 <117087003+Kris2327@users.noreply.github.com>
2026-03-10 16:20:18 +0200

    smth

--- FrontEnd/TeamUp/src/app/pages/log-in/log-in.ts
+++ FrontEnd/TeamUp/src/app/pages/log-in/log-in.ts
@@ -1,11 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginUser, RegisterUser } from '../../services/auth/auth-types';
+import { Auth } from '../../services/auth/auth';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-log-in',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './log-in.html',
   styleUrl: './log-in.css',
 })
 export class LogIn {
+
+  @ViewChild('pageDiv') pageDiv!: ElementRef;
+  userData: LoginUser = {
+    emailOrUsername: '',
+    password: ''
+  };
+
+  showPassword = false;
+  isDarkMode = false;
+
+  constructor(private renderer: Renderer2, private auth: Auth) {}
+
+  ngOnInit() {
+    this.auth.me();
+  }
+
+  ngAfterViewInit() {
+    this.renderer.addClass(this.pageDiv.nativeElement, 'light-mode');
+  }
+
+  toggleDarkMode() {
+    this.isDarkMode = !this.isDarkMode;
+
+    if (this.isDarkMode) {
+      this.renderer.removeClass(this.pageDiv.nativeElement, 'light-mode');
+      this.renderer.addClass(this.pageDiv.nativeElement, 'dark-mode');
+    } else {
+      this.renderer.removeClass(this.pageDiv.nativeElement, 'dark-mode');
+      this.renderer.addClass(this.pageDiv.nativeElement, 'light-mode');
+    }
+  }
+
+  togglePassword() {
+    this.showPassword = !this.showPassword;
+  }
 
 }
