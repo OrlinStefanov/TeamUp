9b5b30b3a157fee9ff2ffdadd0b8dac3d57ce99d
OrlinStefanov <orlinss2008@gmail.com>
2026-03-11 16:53:07 +0200

    changes

--- FrontEnd/TeamUp/src/app/app.routes.ts
+++ FrontEnd/TeamUp/src/app/app.routes.ts
@@ -1,17 +1,13 @@
 import { Routes } from '@angular/router';
 import { SignUp } from './pages/sign-up/sign-up';
+import { StartUp } from './pages/start-up/start-up';
+import { LogIn } from './pages/log-in/log-in';
+import { ForgotPassword } from './pages/forgot-password/forgot-password';
 
 export const routes: Routes = [
-<<<<<<< Updated upstream
-  { 
-    path: '',
-    component: SignUp
-  }
-=======
   { path: '', component: StartUp },
   { path: 'signup', component: SignUp },
   { path: 'login', component: LogIn },
   { path: 'forgot-password', component: ForgotPassword },
-  { path: 'dashboard', component: Dashboard, canActivate: [authGuardGuardGuard] },
->>>>>>> Stashed changes
+  //{ path: 'dashboard', component: Dashboard, canActivate: [authGuardGuardGuard] },
 ];
