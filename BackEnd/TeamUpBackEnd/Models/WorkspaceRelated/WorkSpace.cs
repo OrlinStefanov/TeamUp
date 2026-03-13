ef3918e381215ff6ed15c09b590f89a9192231ce
OrlinStefanov <orlinss2008@gmail.com>
2026-03-13 17:33:48 +0200

    chnages

--- BackEnd/TeamUpBackEnd/Models/WorkspaceRelated/WorkSpace.cs
+++ BackEnd/TeamUpBackEnd/Models/WorkspaceRelated/WorkSpace.cs
@@ -13,8 +13,8 @@ namespace TeamUpBackEnd.Models.WorkspaceRelated
 		public ApplicationUser? Owner { get; set; }
 
 		public DateTime? CreatedAt { get; set; }
-		public ICollection<Channel>? Channels { get; set; }
-		public ICollection<WorkSpaceMember>? Members { get; set; }
-		public ICollection<TaskItem>? Tasks { get; set; }
+		public ICollection<Channel> Channels { get; set; } = new List<Channel>();
+		public ICollection<WorkSpaceMember> Members { get; set; } = new List<WorkSpaceMember>();
+		public ICollection<TaskItem> Tasks { get; set; } = new List<TaskItem>();
 	}
 }
