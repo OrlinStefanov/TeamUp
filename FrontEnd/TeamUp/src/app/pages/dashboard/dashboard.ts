587cad02a319f2dd04a0a3f17f450801b1a40dc5
OrlinStefanov <orlinss2008@gmail.com>
2026-03-14 21:57:46 +0200

    adding backend logic in dashboard p1

--- FrontEnd/TeamUp/src/app/pages/dashboard/dashboard.ts
+++ FrontEnd/TeamUp/src/app/pages/dashboard/dashboard.ts
@@ -2,7 +2,12 @@ import { Component } from '@angular/core';
 import { ElementRef, Renderer2, ViewChild } from '@angular/core';
 import { Auth } from '../../services/auth/auth';
 import { FormsModule } from '@angular/forms';
+<<<<<<< Updated upstream
 import { CommonModule, NgIf } from '@angular/common';
+=======
+import { CommonModule } from '@angular/common';
+import { UserProfile } from '../../services/auth/auth-types';
+>>>>>>> Stashed changes
 
 @Component({
   selector: 'app-dashboard',
@@ -14,11 +19,28 @@ export class Dashboard {
     @ViewChild('pageDiv') pageDiv!: ElementRef;
 
     isDarkMode: boolean = false;
+<<<<<<< Updated upstream
+=======
+    showDropdown = false;
+    showSettingsDropdown = false;
+    showCreateWorkspace = false;
+    timeout: any;
+
+    workspaces : any[] = []; // тук ще държим списъка с workspaces, който ще се зарежда от бекенда
+
+    //orcho added
+    userProfile : any = null;
+>>>>>>> Stashed changes
 
     constructor(private renderer: Renderer2, private auth: Auth) {}
 
     ngOnInit() {
-    this.auth.me();
+    this.auth.me().subscribe(user => {
+      this.userProfile = user;
+      console.log('Current user:', user);
+    });
+
+    this.getWorkspaces();
 
     const savedMode = localStorage.getItem('darkMode');
 
@@ -31,6 +53,13 @@ export class Dashboard {
     } else {
       this.renderer.addClass(this.pageDiv.nativeElement, 'light-mode');
     }
+  }
+
+  getWorkspaces() {
+    this.auth.getWorkspaces().subscribe((response: any) => {
+      this.workspaces = response;
+      console.log('Workspaces:', this.workspaces);
+    });
   }
 
   toggleDarkMode() {
@@ -46,4 +75,65 @@ export class Dashboard {
       this.renderer.addClass(this.pageDiv.nativeElement, 'light-mode');
     }
   }
+<<<<<<< Updated upstream
+=======
+
+  showDropDown() {
+    this.showDropdown = !this.showDropdown;
+  }
+
+  showSettingsDropDown() {
+    this.showSettingsDropdown = !this.showSettingsDropdown;
+  }
+
+  openCreateWorkspace(){
+    this.showCreateWorkspace = true;
+  }
+
+  closeCreateWorkspace(){
+    this.showCreateWorkspace = false;
+  }
+
+  closeMenu() {
+    this.timeout = setTimeout(() => {
+      this.showDropdown = false;
+    }, 90);
+  }
+
+  closeSettingsMenu() {
+    this.timeout = setTimeout(() => {
+      this.showSettingsDropdown = false;
+    }, 90);
+  }
+
+  signOut(){
+    this.auth.logout().subscribe({
+      next: () => {
+        console.log('Logged out successfully');
+        window.location.href = '/dashboard';
+      },
+      error: (error) => {
+        console.error('Logout failed:', error);
+      }
+    });
+  }
+  
+  createWorkspace(){
+    const input = document.getElementsByName("workspaceName")[0] as HTMLInputElement;
+
+    if (!input) return; // ако няма input, спираме
+
+    const value: string = input.value.trim();
+
+    if (value) {
+      this.workspaces.push({ name: value }); // добавяме новия workspace
+      input.value = ''; // чистим input
+      console.log(this.workspaces);
+      this.showCreateWorkspace = false;
+    }
+
+    
+  }
+
+>>>>>>> Stashed changes
 }
