03773d9cb7dd0f21062a9d7bca0c4a7ec1f2c374
OrlinStefanov <orlinss2008@gmail.com>
2026-03-22 23:07:34 +0200

    create channel

---
+++ BackEnd/TeamUpBackEnd/DTO/ChatsDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace TeamUpBackEnd.DTO
+{
+	public class ChatsDTO
+	{
+		public record CreateChatDTO
+		{
+			public string? Name { get; set; }
+			public string? Description { get; set; }
+			public bool IsPrivate { get; set; }
+		}
+	}
+}
